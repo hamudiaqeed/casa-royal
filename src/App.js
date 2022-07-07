a8217de109ec198b9943d726680430857b8fedce
Mohammed Aqeed <hamudy_fun5b@yahoo.com>
2022-07-07 13:38:41 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+import './App.css';
+import Categories from './categories.component';
+
+function App() {
+  return (
+    <div className="App">
+      <Categories />
+    </div>
+  );
+}
+
+export default App;
